327e56c1cf53802fdc47c56a3c30aa10f8744efe
sergiyfulldev <sergiy.romanyuk1@gmail.com>
2020-02-24 16:14:20 +0200

    pockmon game

---
+++ iOS 10/FoodApp/FoodApp/FoodApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  FoodApp
+//
+//  Created by hussien alrubaye on 11/27/16.
+//  Copyright © 2016 hussien alrubaye. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var laName: UILabel!
+    @IBOutlet weak var laImage: UIImageView!
+}
